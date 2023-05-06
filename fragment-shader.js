483f6fdab962e8d8aa6dcfe1bd35ef5446702d96
Taisuke Fukuno <fukuno@jig.jp>
2023-05-06 14:58:01 +0900

    add src attribute to fetch src of GLSL

--- fragment-shader.js
+++ fragment-shader.js
@@ -10,8 +10,9 @@ export class FragmentShader extends HTMLElement {
     this.style.display = "block";
     const canvas = document.createElement("canvas");
     this.appendChild(canvas);
-    //canvas.onclick = () => canvas.requestFullscreen();
-    //document.body.style.margin = 0;
+    if (this.getAttribute("fullscreen") !== null) {
+      this.onclick = () => this.requestFullscreen();
+    }
     const limitw = this.getAttribute("width");
     
     const resizeHandler = () => {
@@ -48,64 +49,77 @@ export class FragmentShader extends HTMLElement {
     }
     `;
     
-    // シェーダ周りの初期化
-    const prg = createProgram(gl, createVertexShader(gl, vs), createFragmentShader(gl, fs));
-    const timeUni = gl.getUniformLocation(prg, "time");
-    const mouseUni = gl.getUniformLocation(prg, "mouse");
-    const resolutionUni = gl.getUniformLocation(prg, "resolution");
-    
-    // 頂点データ回りの初期化
-    const position = [
-      -1.0,  1.0,  0.0,
-       1.0,  1.0,  0.0,
-      -1.0, -1.0,  0.0,
-       1.0, -1.0,  0.0,
-    ];
-    const index = [
-      0, 2, 1,
-      1, 2, 3,
-    ];
-    const vPosition = createVBO(gl, position);
-    const vIndex = createIBO(gl, index);
-    const vAttLocation = gl.getAttribLocation(prg, "position");
-    gl.bindBuffer(gl.ARRAY_BUFFER, vPosition);
-    gl.enableVertexAttribArray(vAttLocation);
-    gl.vertexAttribPointer(vAttLocation, 3, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex);
-    
-    // その他の初期化
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    
-    let mx = 0.5;
-    let my = 0.5;
-    // イベントリスナー登録
-    canvas.addEventListener("mousemove", (e) => {
-      mx = e.offsetX / canvas.width;
-      my = e.offsetY / canvas.height;
-    }, true);
-    
-    const startTime = performance.now();
-    // レンダリングを行う関数
-    const render = () => {
-      // 時間管理
-      const time = (performance.now() - startTime) / 1000;
+    const show = (fs) => {
+      // シェーダ周りの初期化
+      const prg = createProgram(gl, createVertexShader(gl, vs), createFragmentShader(gl, fs));
+      const timeUni = gl.getUniformLocation(prg, "time");
+      const mouseUni = gl.getUniformLocation(prg, "mouse");
+      const resolutionUni = gl.getUniformLocation(prg, "resolution");
       
-      // カラーバッファをクリア
-      gl.clear(gl.COLOR_BUFFER_BIT);
+      // 頂点データ回りの初期化
+      const position = [
+        -1.0,  1.0,  0.0,
+        1.0,  1.0,  0.0,
+        -1.0, -1.0,  0.0,
+        1.0, -1.0,  0.0,
+      ];
+      const index = [
+        0, 2, 1,
+        1, 2, 3,
+      ];
+      const vPosition = createVBO(gl, position);
+      const vIndex = createIBO(gl, index);
+      const vAttLocation = gl.getAttribLocation(prg, "position");
+      gl.bindBuffer(gl.ARRAY_BUFFER, vPosition);
+      gl.enableVertexAttribArray(vAttLocation);
+      gl.vertexAttribPointer(vAttLocation, 3, gl.FLOAT, false, 0, 0);
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex);
       
-      // uniform 関連
-      gl.uniform1f(timeUni, time);
-      gl.uniform2fv(mouseUni, [mx, my]);
-      gl.uniform2fv(resolutionUni, [canvas.width, canvas.height]);
+      // その他の初期化
+      gl.clearColor(0.0, 0.0, 0.0, 1.0);
       
-      // 描画
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-      gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-      gl.flush();
+      let mx = 0.5;
+      let my = 0.5;
+      // イベントリスナー登録
+      canvas.addEventListener("mousemove", (e) => {
+        mx = e.offsetX / canvas.width;
+        my = e.offsetY / canvas.height;
+      }, true);
       
-      requestAnimationFrame(render);
+      const startTime = performance.now();
+      // レンダリングを行う関数
+      const render = () => {
+        // 時間管理
+        const time = (performance.now() - startTime) / 1000;
+        
+        // カラーバッファをクリア
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        
+        // uniform 関連
+        gl.uniform1f(timeUni, time);
+        gl.uniform2fv(mouseUni, [mx, my]);
+        gl.uniform2fv(resolutionUni, [canvas.width, canvas.height]);
+        
+        // 描画
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+        gl.flush();
+        
+        requestAnimationFrame(render);
+      };
+      render();
     };
-    render();    
+    if (fs) {
+      show(fs);
+    } else {
+      const src = this.getAttribute("src");
+      if (src) {
+        (async () => {
+          const fs = await (await fetch(src)).text();
+          show(fs);
+        })();
+      }
+    }
   }
 };
 
