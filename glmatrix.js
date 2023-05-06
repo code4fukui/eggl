ac601f0db1caeb8cfba76689a141b2e433a11f56
Taisuke Fukuno <fukuno@jig.jp>
2023-05-06 17:45:08 +0900

    fix perspective angle

---
+++ glmatrix.js
@@ -0,0 +1,6 @@
+import * as mat4 from "https://glmatrix.net/src/mat4.js";
+import * as vec4 from "https://glmatrix.net/src/vec4.js";
+import * as vec3 from "https://glmatrix.net/src/vec3.js";
+import * as quat from "https://glmatrix.net/src/quat.js";
+
+export { mat4, vec4, vec3, quat };
